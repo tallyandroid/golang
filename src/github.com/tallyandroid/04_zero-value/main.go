3db8940999c2760e61eab888c845fd29956aa3ea
Christian Tisby <christiantisby@Christians-MBP.hsd1.fl.comcast.net>
2020-05-14 23:47:02 -0400

    adding zero type

---
+++ src/github.com/tallyandroid/04_zero-value/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+var y string
+
+func main() {
+	//  DECLARE a variable to be of a certain type
+	// and then ASSIGN A VALUE of that type to the variable
+
+	fmt.Println("printing the value of y", y, "ending")
+	fmt.Printf("%T", y)
+
+}
